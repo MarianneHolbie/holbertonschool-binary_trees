a5cfaa801352cb75f3668c40f7e914cd69e01b54
Arrue Marianne <5608@holbertonstudents.com>
2023-01-10 02:21:55 -0800

    add if condition to follow branch

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,9 +13,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-
-	left_height = binary_tree_height(tree->left) + 1;
-	right_height = binary_tree_height(tree->right) + 1;
+	if (tree->left)
+		left_height = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right_height = binary_tree_height(tree->right) + 1;
 
 	if (left_height > right_height)
 		return (left_height);
