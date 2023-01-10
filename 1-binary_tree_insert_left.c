3c4dcab68fdc5f038bc781f9712f31b529366dbf
Arrue Marianne <5608@holbertonstudents.com>
2023-01-10 01:21:26 -0800

    test Hugo code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,33 +10,25 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_tree_left, *tmp;
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
-	new_tree_left = malloc(sizeof(binary_tree_t));
-
-	if (new_tree_left == NULL || parent == NULL)
+	if (!parent || !new)
 	{
-		free(new_tree_left);
+		free(new);
 		return (NULL);
 	}
 
+	new->parent = parent;
+	new->n = value;
+	new->right = NULL;
+	new->left = NULL;
+
 	if (parent->left)
 	{
-		tmp = parent->left;
-		new_tree_left->parent = parent;
-		new_tree_left->n = value;
-		new_tree_left->left = tmp;
-		parent->left = new_tree_left;
-		tmp->parent = new_tree_left;
-		return (new_tree_left);
-
-	}
-	else
-	{
-		new_tree_left->parent = parent;
-		new_tree_left->n = value;
-		parent->left = new_tree_left;
+		new->left = parent->left;
+		new->left->parent = new;
 	}
 
-	return (new_tree_left);
+	parent->left = new;
+	return (new);
 }
