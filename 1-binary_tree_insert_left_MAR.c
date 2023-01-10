9cc9fc080a7e21cb3daf80ac2f54cc50f2662871
Arrue Marianne <5608@holbertonstudents.com>
2023-01-10 00:57:33 -0800

    cleaning !

---
+++ 1-binary_tree_insert_left_MAR.c
@@ -0,0 +1,50 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left- insert left-child node
+ * @parent: parent to insert
+ * @value: value of new node
+ *
+ * Return: pointer to created node or NULL
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_tree_left, *tmp;
+
+    new_tree_left = malloc(sizeof(binary_tree_t));
+
+	if (new_tree_left == NULL)
+	{
+		fprintf(stderr, "Erreur d'allocation memoire.\n");
+        free(new_tree_left);
+		return (NULL);
+	}
+
+
+	if (parent == NULL)
+    {
+		return (NULL);
+    }
+
+	
+	if (parent->left)
+	{
+		tmp = parent->left;
+		new_tree_left->parent = parent;
+		new_tree_left->n = value;
+		new_tree_left->left = tmp;
+		parent->left = new_tree_left;
+		tmp->parent = new_tree_left;
+		return (new_tree_left);
+
+	}
+	else
+	{
+		new_tree_left->parent = parent;
+		new_tree_left->n = value;
+		parent->left = new_tree_left;
+	}
+
+	return (new_tree_left);
+}
